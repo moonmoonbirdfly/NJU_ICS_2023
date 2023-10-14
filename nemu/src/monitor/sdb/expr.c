3d87cb68cca62084c6d5e697b3364dc3458807fd
tracer-ics2023 <tracer@njuics.org>
2023-10-14 22:17:44 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:44 up  5:18,  1 user,  load average: 0.37, 0.24, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -145,96 +145,146 @@ static int find_major(int p, int q) {
 }
 
 
-static word_t eval_operand(int i, bool *ok) {
-  switch (tokens[i].type) {
+// 定义一个名为eval_operand的函数。这个函数接收两个参数：一个整数i和一个布尔指针ok。
+// 函数返回一个名为word_t的数据类型的值，该数据类型未在代码中定义，可能已在其他地方定义。
+static word_t eval_operand(int i, bool *ok) { 
+
+  // 根据第i个令牌的类型执行相应的操作
+  switch (tokens[i].type) { 
+
+  // 如果令牌类型为TK_NUM（可能表示这是一个数字）
   case TK_NUM:
+
+    // 判断字符串的前两个字符是否为 '0x'，这通常表示一个十六进制数
     if (strncmp("0x", tokens[i].str, 2) == 0) return strtol(tokens[i].str, NULL, 16); 
+
+    // 否则，将字符串转换为十进制数
     else return strtol(tokens[i].str, NULL, 10);
+
+  // 如果令牌类型为TK_REG（可能表示这是一个寄存器）
   case TK_REG:
+
+    // 将字符串值转换为寄存器对应的数值
     return isa_reg_str2val(tokens[i].str, ok);
+
+  // 对于不满足以上几种情况的令牌
   default:
+
+    // 将ok设置为false，表示失败
     *ok = false;
+    
+    // 返回0
     return 0;
   }
 }
 
-// unary operator
+// 定义一个处理一元操作的函数。函数接收一个操作符op、一个值val以及一个布尔指针ok
 static word_t calc1(int op, word_t val, bool *ok) {
-  switch (op)
-  {
+  
+  // 根据操作符对值执行对应的操作
+  switch (op) {
+
+  // 如果是取负操作（TK_NEG可能代表取负），返回值的负数
   case TK_NEG: return -val;
+
+  // 如果是取正操作（TK_POS可能代表取正），返回原值
   case TK_POS: return val;
+
+  // 如果是解引用操作（TK_DEREF可能代表解引用），读取地址为val，大小为8字节的内存值
   case TK_DEREF: return vaddr_read(val, 8);
+
+  // 对于其他未列出操作，将ok设置为false，表示失败
   default: *ok = false;
   }
+
+  // 默认返回0
   return 0;
 }
 
-// binary operator
+// 定义一个处理二元操作的函数。函数接收两个值val1和val2、一个操作符op以及一个布尔指针ok
 static word_t calc2(word_t val1, int op, word_t val2, bool *ok) {
+  
   switch(op) {
-  case '+': return val1 + val2;
-  case '-': return val1 - val2;
-  case '*': return val1 * val2;
-  case '/': if (val2 == 0) {
-    *ok = false;
-    return 0;
-  } 
-  return (sword_t)val1 / (sword_t)val2; // e.g. -1/2, may not pass the expr test
-  case TK_AND: return val1 && val2;
-  case TK_OR: return val1 || val2;
-  case TK_EQ: return val1 == val2;
-  case TK_NEQ: return val1 != val2;
-  case TK_GT: return val1 > val2;
-  case TK_LT: return val1 < val2;
-  case TK_GE: return val1 >= val2;
-  case TK_LE: return val1 <= val2;
-  default: *ok = false; return 0;
+    
+  case '+': return val1 + val2;   // 加法操作
+  case '-': return val1 - val2;   // 减法操作
+  case '*': return val1 * val2;   // 乘法操作
+  case '/':                       // 除法操作
+    if (val2 == 0) {              // 检查除数是否为0，如果为0则会导致除法错误
+      *ok = false;                // 将ok设置为false，表示失败
+      return 0;                   // 返回0
+    } 
+    return (sword_t)val1 / (sword_t)val2; // 如果除数不为0，进行除法操作
+                                          // 注意强制性的类型转换，可能是为了保证结果精度或者进行符号扩展
+  case TK_AND: return val1 && val2;                    // 逻辑与操作
+  case TK_OR: return val1 || val2;                     // 逻辑或操作
+  case TK_EQ: return val1 == val2;                     // 等于操作
+  case TK_NEQ: return val1 != val2;                    // 不等于操作
+  case TK_GT: return val1 > val2;                      // 大于操作
+  case TK_LT: return val1 < val2;                      // 小于操作
+  case TK_GE: return val1 >= val2;                     // 大于等于操作
+  case TK_LE: return val1 <= val2;                     // 小于等于操作
+  default: *ok = false; return 0;                      // 对于其他未列出操作，将ok设置为false，表示失败
+                                                        // 并返回0
   }
 }
+
 
 bool check_parentheses(int p, int q) {
+  // 检查一个子表达式的开始和结尾是否为配对的括号
   if (tokens[p].type=='(' && tokens[q].type==')') {
-    int par = 0;
-    for (int i = p; i <= q; i++) {
-      if (tokens[i].type=='(') par++;
-      else if (tokens[i].type==')') par--;
+    int par = 0; // 用于检查括号是否配对的计数器
 
-      if (par == 0) return i==q; // the leftest parenthese is matched
+    for (int i = p; i <= q; i++) {
+      // 遍历子表达式中的所有符号
+      if (tokens[i].type=='(') par++; // 如果当前符号是左括号，计数器加1
+      else if (tokens[i].type==')') par--; // 如果当前符号是右括号，计数器减1
+
+      // 如果计数器在还没有遍历完所有符号前归零，说明左括号有匹配但后续还有额外的右括号，即括号不配对
+      if (par == 0) return i==q; // 如果匹配的右括号是子表达式的最后一个符号，返回true，否则返回false
     }
   }
-  return false;
+  return false; // 如果子表达式的开始和结尾不是配对的括号，返回false
 }
-
 
 static word_t eval(int p, int q, bool *ok) {
   *ok = true;
   if (p > q) {
+    // 如果子表达式无效（比如空或开始索引大于结束索引），返回错误
     *ok = false;
     return 0;
   } else if (p == q) {
-    return eval_operand(p, ok);
+    // 如果子表达式只有一个字符，直接对其求值
+    return eval_operand(p, ok); 
   } else if (check_parentheses(p, q)) {
+    // 如果子表达式是一个带有配对括号的有效表达式，去掉括号后继续求值
     return eval(p+1, q-1, ok);
   } else {    
+    // 寻找子表达式中运算优先级最低的运算符（如果存在的话）
     int major = find_major(p, q);
     if (major < 0) {
+      // 如果子表达式中没有合法的运算符，返回错误
       *ok = false;
       return 0;
     }
 
+    // 对运算符左边的子表达式进行求值
     bool ok1, ok2;
     word_t val1 = eval(p, major-1, &ok1);
+    // 对运算符右边的子表达式进行求值
     word_t val2 = eval(major+1, q, &ok2);
 
     if (!ok2) {
+      // 如果右边的子表达式不能正确求值，返回错误
       *ok = false;
       return 0;
     }
     if (ok1) {
+      // 如果左边的子表达式也能正确求值，对两个子表达式进行相应的二元运算，返回结果
       word_t ret = calc2(val1, tokens[major].type, val2, ok);
       return ret;
     } else {
+      // 如果左边的子表达式不能正确求值，对右边的子表达式进行一元运算，返回结果
       word_t ret =  calc1(tokens[major].type, val2, ok);
       return ret;
     }
@@ -242,58 +292,74 @@ static word_t eval(int p, int q, bool *ok) {
 }
 
 
+
+// 定义一个函数，输入参数是一个字符指针，返回一个布尔值
 static bool make_token(char *e) {
+  // 定义并初始化位置变量
   int position = 0;
   int i;
   regmatch_t pmatch;
 
+  // 初始化词法标记数量为0
   nr_token = 0;
+  // 当遇到字符串结束符（'\0'）时停止循环
   while (e[position] != '\0') {
     /* Try all rules one by one. */
+    // 尝试所有的规则
     for (i = 0; i < NR_REGEX; i ++) {
+      // 使用正则表达式库函数regexec判断当前字符是否符合规则
       int reg_res = regexec(&re[i], e + position, 1, &pmatch, 0);
+      // 如果匹配且匹配的起始位置为0，执行以下代码
       if (reg_res == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // 采用注释的这段代码用于打印一些调试信息
         
+        // 当前位置前进匹配的字符串长度
         position += substr_len;
         
+        // 如果当前规则的token_type是TK_NOTYPE，则跳过此次循环
         if (rules[i].token_type == TK_NOTYPE) break;
 
+        // 取出还没被赋值的token，将其类型设置为匹配到的类型
         tokens[nr_token].type = rules[i].token_type;
+        // 匹配到的token数目加一
         nr_token++;
-switch (rules[i].token_type) {
-  case TK_NUM:
-  case TK_REG:
-    // todo: handle overflow (token exceeding size of 32B)
-    strncpy(tokens[nr_token].str, substr_start, substr_len);
-    tokens[nr_token].str[substr_len] = '\0';
-    break;
-  case '*': case '-': case '+':
-    if (nr_token==0 || !OFTYPES(tokens[nr_token-1].type, bound_types)) {
-      switch (rules[i].token_type)
-      {
-        case '-': tokens[nr_token].type = TK_NEG; break;
-        case '+': tokens[nr_token].type = TK_POS; break;
-        case '*': tokens[nr_token].type = TK_DEREF; break;
-      }
-    }
-    break;
-}
+
+        // 根据不同的token_type做不同的处理
+        switch (rules[i].token_type) {
+          case TK_NUM:
+          case TK_REG:
+            // 如果token类型是TK_NUM或TK_REG，将匹配到的字符串复制到token.str中
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            break;
+          case '*': case '-': case '+':
+            // 如果当前token是第一个token或之前的token的类型是bound_types，则将这些token的类型做更改
+            if (nr_token==0 || !OFTYPES(tokens[nr_token-1].type, bound_types)) {
+              switch (rules[i].token_type)
+              {
+                case '-': tokens[nr_token].type = TK_NEG; break;
+                case '+': tokens[nr_token].type = TK_POS; break;
+                case '*': tokens[nr_token].type = TK_DEREF; break;
+              }
+            }
+            break;
+        }
 
         break;
       }
     }
 
+    // 如果遍历所有的规则后都没有匹配的，打印错误信息并返回false
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
   }
 
+  // 如果处理完所有字符，返回true
   return true;
 }
 
@@ -301,19 +367,21 @@ switch (rules[i].token_type) {
 
 
 
- 
-
-
-
-
-
+// 函数 'expr' 接收一个字符指针 'e' 和一个 bool 指针 'success' 作为输入
+// 'e' 可能是一个运算表达式的字符串形式，例如 "2+3*4"
+// 'success' 用于让该函数可以将运算成功与否的信息传回调用它的函数
 word_t expr(char *e, bool *success) {
+  
+  // 'make_token' 函数可能是用于将输入的表达式字符串 'e' 转换为一种内部表示（如标记序列）
+  // 如果转换失败（例如，输入不是一个有效的表达式），'make_token' 将返回 false
   if (!make_token(e)) {
+    // 如果'tokenise'返回false，则在'*success'中存放false（表示解析失败），并返回0作为函数结果
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  
-  return eval(0, nr_token-1, success);;
+
+
+  return eval(0, nr_token-1, success);
 }
+
