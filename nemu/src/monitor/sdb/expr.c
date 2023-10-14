25d770de2bd9e6e4171fb4ebc8ae9744f5f29447
tracer-ics2023 <tracer@njuics.org>
2023-10-14 18:02:43 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:43 up  1:03,  1 user,  load average: 0.11, 0.18, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -22,7 +22,9 @@
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
-
+TK_NUM, // 10 & 16
+  TK_REG,
+  TK_VAR,
   /* TODO: Add more token types */
 
 };
@@ -39,6 +41,15 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  {"-", '-'},
+  {"\\*", '*'},
+  {"/", '/'},
+  {"\\(", '('},
+  {"\\)", ')'},
+
+  {"[0-9]+", TK_NUM}, // TODO: non-capture notation (?:pattern) makes compilation failed
+  {"\\$\\w+", TK_REG},
+  {"[A-Za-z_]\\w*", TK_VAR},
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -80,12 +91,13 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
+//	int reg_res=regexec(&re[i],e+position,1,&pmatch,0);
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //    i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
@@ -93,11 +105,17 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+	if (rules[i].token_type == TK_NOTYPE) break;
+	tokens[nr_token].type = rules[i].token_type;
         switch (rules[i].token_type) {
-          default: TODO();
+		case TK_NUM:
+		case TK_REG:
+		case TK_VAR:
+			strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+		//  default: TODO();
         }
-
+	nr_token++;
         break;
       }
     }
@@ -110,6 +128,92 @@ static bool make_token(char *e) {
 
   return true;
 }
+ int find_major(int p, int q) {
+   int ret = -1, par = 0, op_type = 0;
+   for (int i = p; i <= q; i++) {
+     if (tokens[i].type == TK_NUM) {
+       continue;
+     }
+     if (tokens[i].type == '(') {
+       par++;
+    } else if (tokens[i].type == ')') {
+       if (par == 0) {
+         return -1;
+       }
+       par--;
+     } else if (par > 0) {
+       continue;
+     } else {
+       int tmp_type = 0;
+       switch (tokens[i].type) {
+       case '*': case '/': tmp_type = 1; break;
+       case '+': case '-': tmp_type = 2; break;
+       default: assert(0);
+       }
+       if (tmp_type >= op_type) {
+         op_type = tmp_type;
+         ret = i;
+       }
+     }
+   }
+   if (par != 0) return -1;
+   return ret;
+}    
+
+ bool check_parentheses(int p, int q) {
+   if (tokens[p].type=='(' && tokens[q].type==')') {
+     int par = 0;
+     for (int i = p; i <= q; i++) {
+       if (tokens[i].type=='(') par++;
+       else if (tokens[i].type==')') par--;
+
+       if (par == 0) return i==q; // the leftest parenthese is matched
+     }
+   }
+   return false;
+ }
+
+
+sword_t eval(int p, int q, bool *ok) {
+  *ok = true;
+  if (p > q) {
+    *ok = false;
+    return 0;
+  } else if (p == q) {
+    if (tokens[p].type != TK_NUM) {
+      *ok = false;
+      return 0;
+    }
+    word_t ret = strtol(tokens[p].str, NULL, 10);
+    return ret;
+  } else if (check_parentheses(p, q)) {
+    return eval(p+1, q-1, ok);
+  } else {    
+    int major = find_major(p, q);
+    if (major < 0) {
+      *ok = false;
+      return 0;
+    }
+
+    word_t val1 = eval(p, major-1, ok);
+    if (!*ok) return 0;
+    word_t val2 = eval(major+1, q, ok);
+    if (!*ok) return 0;
+    
+    switch(tokens[major].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': if (val2 == 0) {
+        *ok = false;
+        return 0;
+      } 
+      return (sword_t)val1 / (sword_t)val2; // e.g. -1/2, may not pass the expr test
+      default: assert(0);
+    }
+  }
+}
+
 
 
 word_t expr(char *e, bool *success) {
@@ -119,7 +223,6 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  
+  return eval(0, nr_token-1, success);
 }
