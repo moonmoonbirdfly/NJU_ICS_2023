39228ea6dab4570af692e906ddc4f86985fcf3cb
tracer-ics2023 <tracer@njuics.org>
2023-10-15 22:41:52 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:52 up  1:34,  1 user,  load average: 0.22, 0.16, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -222,22 +222,17 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     // deal with negative number
     if(e[position] == '-') {
-    if (position == 0 || e[position-1] == '+' || e[position-1] == '-' ||
-        e[position-1] == '*' || e[position-1] == '/' || e[position-1] == '(') {
-        tokens[nr_token].type = TK_NEG;
-        tokens[nr_token].str[0] = e[position+1];
-        tokens[nr_token].str[1] = '\0'; 
-        nr_token++;
-        position += 2;
-        continue;
+    tokens[nr_token].str[0] = e[position]; 
+    tokens[nr_token].str[1] = '\0'; 
+    if (e[position+1] == '-') { // check if immediate next character is also a minus
+      tokens[nr_token].type = TK_NEG;
+      position += 2;
     } else {
       tokens[nr_token].type = '-';
-      tokens[nr_token].str[0] = e[position]; 
-      tokens[nr_token].str[1] = '\0'; 
-      nr_token++;
       position++;
-      continue;
-    } 
+    }
+    nr_token++;
+    continue;
   }
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
