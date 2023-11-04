17a5e53091a423750395918ff577ab7986519194
tracer-ics2023 <tracer@njuics.org>
2023-11-04 21:22:54 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:54 up  1:48,  1 user,  load average: 0.11, 0.14, 0.14

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -28,28 +28,33 @@
 // calculate the length of an array
 #define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
 
-// macro concatenation
-#define concat_temp(x, y) x ## y
-#define concat(x, y) concat_temp(x, y)
-#define concat3(x, y, z) concat(concat(x, y), z)
-#define concat4(x, y, z, w) concat3(concat(x, y), z, w)
-#define concat5(x, y, z, v, w) concat4(concat(x, y), z, v, w)
+// 宏连接
+#define concat_temp(x, y) x ## y // 连接两个宏参数
+#define concat(x, y) concat_temp(x, y) // 间接连接两个宏参数
+#define concat3(x, y, z) concat(concat(x, y), z) // 连接三个宏参数
+#define concat4(x, y, z, w) concat3(concat(x, y), z, w) // 连接四个宏参数
+#define concat5(x, y, z, v, w) concat4(concat(x, y), z, v, w) // 连接五个宏参数
 
 // macro testing
 // See https://stackoverflow.com/questions/26099745/test-if-preprocessor-symbol-is-defined-inside-macro
-#define CHOOSE2nd(a, b, ...) b
-#define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
-#define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
-// define placeholders for some property
-#define __P_DEF_0  X,
-#define __P_DEF_1  X,
-#define __P_ONE_1  X,
-#define __P_ZERO_0 X,
-// define some selection functions based on the properties of BOOLEAN macro
-#define MUXDEF(macro, X, Y)  MUX_MACRO_PROPERTY(__P_DEF_, macro, X, Y)
-#define MUXNDEF(macro, X, Y) MUX_MACRO_PROPERTY(__P_DEF_, macro, Y, X)
-#define MUXONE(macro, X, Y)  MUX_MACRO_PROPERTY(__P_ONE_, macro, X, Y)
-#define MUXZERO(macro, X, Y) MUX_MACRO_PROPERTY(__P_ZERO_,macro, X, Y)
+#define CHOOSE2nd(a, b, ...) b // CHOOSE2nd宏用于选择传入参数的第二个参数。
+
+#define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b) // MUX_WITH_COMMA宏通过检查contain_comma是否包含逗号来决定是选择a还是b。
+
+#define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b) // MUX_MACRO_PROPERTY宏结合了宏属性和MUX_WITH_COMMA的选择机制。
+
+// 定义一些属性的占位符
+#define __P_DEF_0  X, // __P_DEF_0是一个占位宏，用于当宏没有定义时的情况。
+#define __P_DEF_1  X, // __P_DEF_1是一个占位宏，用于当宏已定义时的情况。
+#define __P_ONE_1  X, // __P_ONE_1是一个占位宏，用于特定宏的值为1时的情况。
+#define __P_ZERO_0 X, // __P_ZERO_0是一个占位宏，用于特定宏的值为0时的情况。
+
+// 根据BOOLEAN宏的属性定义一些选择函数
+#define MUXDEF(macro, X, Y)  MUX_MACRO_PROPERTY(__P_DEF_, macro, X, Y) // MUXDEF用于选择当宏macro定义了时的值X，否则为Y。
+#define MUXNDEF(macro, X, Y) MUX_MACRO_PROPERTY(__P_DEF_, macro, Y, X) // MUXNDEF用于选择当宏macro没有定义时的值X，否则为Y。
+#define MUXONE(macro, X, Y)  MUX_MACRO_PROPERTY(__P_ONE_, macro, X, Y) // MUXONE用于选择当宏macro的值为1时的值X，否则为Y。
+#define MUXZERO(macro, X, Y) MUX_MACRO_PROPERTY(__P_ZERO_, macro, X, Y) // MUXZERO用于选择当宏macro的值为0时的值X，否则为Y。
+
 
 // test if a boolean macro is defined
 #define ISDEF(macro) MUXDEF(macro, 1, 0)
@@ -64,16 +69,16 @@
 // NOTE2: macros defined to themselves (#define A A) will get wrong results
 #define isdef(macro) (strcmp("" #macro, "" str(macro)) != 0)
 
-// simplification for conditional compilation
-#define __IGNORE(...)
-#define __KEEP(...) __VA_ARGS__
-// keep the code if a boolean macro is defined
+// 宏定义条件编译简化
+#define __IGNORE(...) // 忽略参数
+#define __KEEP(...) __VA_ARGS__ // 保持参数
+// 如果宏定义了，就保留代码
 #define IFDEF(macro, ...) MUXDEF(macro, __KEEP, __IGNORE)(__VA_ARGS__)
-// keep the code if a boolean macro is undefined
+// 如果宏没定义，就保留代码
 #define IFNDEF(macro, ...) MUXNDEF(macro, __KEEP, __IGNORE)(__VA_ARGS__)
-// keep the code if a boolean macro is defined to 1
+// 如果宏定义为1，就保留代码
 #define IFONE(macro, ...) MUXONE(macro, __KEEP, __IGNORE)(__VA_ARGS__)
-// keep the code if a boolean macro is defined to 0
+// 如果宏定义为0，就保留代码
 #define IFZERO(macro, ...) MUXZERO(macro, __KEEP, __IGNORE)(__VA_ARGS__)
 
 // functional-programming-like macro (X-macro)
@@ -81,6 +86,8 @@
 // NOTE1: `c` should be defined as a list like:
 //   f(a0) f(a1) f(a2) ...
 // NOTE2: each element in the container can be a tuple
+// 函数式编程风格的宏（X-macro）
+// 将函数`f`应用于容器`c`中的每个元素
 #define MAP(c, f) c(f)
 
 #define BITMASK(bits) ((1ull << (bits)) - 1) // 创建位掩码
