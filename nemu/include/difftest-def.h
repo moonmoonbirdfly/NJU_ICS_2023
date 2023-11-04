b2642ca245d4db71ace61eb1acb1b801353e164f
tracer-ics2023 <tracer@njuics.org>
2023-11-04 21:35:49 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:49 up  2:01,  1 user,  load average: 0.19, 0.18, 0.17

--- nemu/include/difftest-def.h
+++ nemu/include/difftest-def.h
@@ -16,25 +16,34 @@
 #ifndef __DIFFTEST_DEF_H__
 #define __DIFFTEST_DEF_H__
 
-#include <stdint.h>
-#include <macro.h>
-#include <generated/autoconf.h>
+#include <stdint.h> // 包含标准整型定义
+#include <macro.h>  // 包含宏定义文件，假设定义了MUXDEF等宏
+#include <generated/autoconf.h> // 包含生成的自动配置头文件
 
-#define __EXPORT __attribute__((visibility("default")))
+#define __EXPORT __attribute__((visibility("default"))) // 设置默认的符号可见性为导出
+
+// 定义枚举，用于指明差异测试的方向，DIFFTEST_TO_DUT 表示测试数据发送到被测试设计，DIFFTEST_TO_REF 表示发送到参考模型
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 
-#if defined(CONFIG_ISA_x86)
-# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 9) // GPRs + pc
-#elif defined(CONFIG_ISA_mips32)
-# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 38) // GPRs + status + lo + hi + badvaddr + cause + pc
-#elif defined(CONFIG_ISA_riscv)
-#define RISCV_GPR_TYPE MUXDEF(CONFIG_RV64, uint64_t, uint32_t)
-#define RISCV_GPR_NUM  MUXDEF(CONFIG_RVE , 16, 32)
-#define DIFFTEST_REG_SIZE (sizeof(RISCV_GPR_TYPE) * (RISCV_GPR_NUM + 1)) // GPRs + pc
-#elif defined(CONFIG_ISA_loongarch32r)
-# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 33) // GPRs + pc
+#if defined(CONFIG_ISA_x86) // 如果定义了 CONFIG_ISA_x86，即x86架构
+# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 9) // x86架构的GPRs加上程序计数器pc的大小
+
+#elif defined(CONFIG_ISA_mips32) // 如果定义了 CONFIG_ISA_mips32，即MIPS32架构
+# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 38) // MIPS32架构的GPRs加上状态寄存器status等的大小
+
+#elif defined(CONFIG_ISA_riscv) // 如果定义了 CONFIG_ISA_riscv，即RISC-V架构
+#define RISCV_GPR_TYPE MUXDEF(CONFIG_RV64, uint64_t, uint32_t) // 根据是否定义了 CONFIG_RV64，选择适合的通用寄存器类型
+#define RISCV_GPR_NUM  MUXDEF(CONFIG_RVE , 16, 32) // 根据是否定义了 CONFIG_RVE，选择通用寄存器的数量
+#define DIFFTEST_REG_SIZE (sizeof(RISCV_GPR_TYPE) * (RISCV_GPR_NUM + 1)) // RISC-V架构的GPRs加上程序计数器pc的大小
+
+#elif defined(CONFIG_ISA_loongarch32r) // 如果定义了 CONFIG_ISA_loongarch32r，即龙芯32位R型架构
+# define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 33) // 龙芯32位R型架构的GPRs加上程序计数器pc的大小
+
 #else
-# error Unsupport ISA
+# error Unsupport ISA // 如果没有支持的ISA定义，抛出错误
 #endif
 
-#endif
+#endif // __DIFFTEST_DEF_H__
+
+
+
