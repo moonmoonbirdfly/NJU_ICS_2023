31123c4960f9757f88e2ebda6af3d6e767d699ed
tracer-ics2023 <tracer@njuics.org>
2023-11-05 20:09:21 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:21 up  1:22,  1 user,  load average: 0.12, 0.14, 0.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -13,64 +13,68 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "local-include/reg.h"
-#include <cpu/cpu.h>
-#include <cpu/ifetch.h>
-#include <cpu/decode.h>
+#include "local-include/reg.h" // 包含一些寄存器的定义和操作
+#include <cpu/cpu.h> // 包含 CPU 的结构和函数
+#include <cpu/ifetch.h> // 包含指令取址的函数
+#include <cpu/decode.h> // 包含指令解码的函数
 
-#define R(i) gpr(i)
-#define Mr vaddr_read
-#define Mw vaddr_write
+#define R(i) gpr(i) // 定义一个宏，用于访问通用寄存器的值
+#define Mr vaddr_read // 定义一个宏，用于读取虚拟地址的内容
+#define Mw vaddr_write // 定义一个宏，用于写入虚拟地址的内容
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
   TYPE_N, // none
-};
+}; // 定义一个枚举类型，用于表示不同的指令格式
 
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define src1R() do { *src1 = R(rs1); } while (0) // 定义一个宏，用于获取源寄存器 1 的值
+#define src2R() do { *src2 = R(rs2); } while (0) // 定义一个宏，用于获取源寄存器 2 的值
+#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0) // 定义一个宏，用于获取 I 型指令的立即数
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0) // 定义一个宏，用于获取 U 型指令的立即数
+#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0) // 定义一个宏，用于获取 S 型指令的立即数
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
-  uint32_t i = s->isa.inst.val;
-  int rs1 = BITS(i, 19, 15);
-  int rs2 = BITS(i, 24, 20);
-  *rd     = BITS(i, 11, 7);
-  switch (type) {
-    case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
-    case TYPE_S: src1R(); src2R(); immS(); break;
+  // 定义一个静态函数，用于解码指令的操作数
+  uint32_t i = s->isa.inst.val; // 获取指令的原始值
+  int rs1 = BITS(i, 19, 15); // 获取源寄存器 1 的编号
+  int rs2 = BITS(i, 24, 20); // 获取源寄存器 2 的编号
+  *rd     = BITS(i, 11, 7); // 获取目标寄存器的编号
+  switch (type) { // 根据指令的格式，获取不同的操作数
+    case TYPE_I: src1R();          immI(); break; // I 型指令，获取源寄存器 1 和立即数
+    case TYPE_U:                   immU(); break; // U 型指令，获取立即数
+    case TYPE_S: src1R(); src2R(); immS(); break; // S 型指令，获取源寄存器 1、源寄存器 2 和立即数
   }
 }
 
 static int decode_exec(Decode *s) {
-  int rd = 0;
-  word_t src1 = 0, src2 = 0, imm = 0;
-  s->dnpc = s->snpc;
+  // 定义一个静态函数，用于解码和执行指令
+  int rd = 0; // 定义一个变量，用于存放目标寄存器的编号
+  word_t src1 = 0, src2 = 0, imm = 0; // 定义三个变量，用于存放操作数的值
+  s->dnpc = s->snpc; // 设置下一条指令的地址
 
-#define INSTPAT_INST(s) ((s)->isa.inst.val)
+#define INSTPAT_INST(s) ((s)->isa.inst.val) // 定义一个宏，用于获取指令的原始值
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
-}
+} // 定义一个宏，用于匹配指令的名称和格式，并执行相应的操作
 
-  INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
-  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
-  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
+  INSTPAT_START(); // 定义一个宏，用于开始匹配指令
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm); // 匹配 auipc 指令，执行 R(rd) = s->pc + imm 的操作
+  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1)); // 匹配 lbu 指令，执行 R(rd) = Mr(src1 + imm, 1) 的操作
+  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2)); // 匹配 sb 指令，执行 Mw(src1 + imm, 1, src2) 的操作
 
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-  INSTPAT_END();
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0 // 匹配 ebreak 指令，执行 NEMUTRAP(s->pc, R(10)) 的操作，其中 R(10) 是 $a0
+  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc)); // 匹配无效指令，执行 INV(s->pc) 的操作
+  INSTPAT_END(); // 定义一个宏，用于结束匹配指令
 
-  R(0) = 0; // reset $zero to 0
+  R(0) = 0; // reset $zero to 0 // 把寄存器 0 的值重置为 0
 
-  return 0;
+  return 0; // 返回 0
 }
 
 int isa_exec_once(Decode *s) {
-  s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  return decode_exec(s);
+  // 定义一个函数，用于执行一条指令
+  s->isa.inst.val = inst_fetch(&s->snpc, 4); // 从内存中取出一条指令，长度为 4 字节
+  return decode_exec(s); // 调用 decode_exec 函数，解码和执行指令
 }
+
