c58a76cadeb671b2fde7eced8c8b9af9f171b7f9
tracer-ics2023 <tracer@njuics.org>
2023-11-05 19:59:07 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:59:07 up  1:12,  1 user,  load average: 0.27, 0.19, 0.12

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -18,18 +18,21 @@
 
 #include <common.h>
 
+// 定义一个结构体，用于存储 RISC-V CPU 的状态
 typedef struct {
-  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
-  vaddr_t pc;
+  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)]; // 通用寄存器数组，其大小根据 CONFIG_RVE 的值来确定（如果 CONFIG_RVE 为真，大小为16；否则为32）
+  vaddr_t pc; // 程序计数器
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
-// decode
+// 定义一个结构体，用于存储指令解码相关的信息
 typedef struct {
   union {
     uint32_t val;
   } inst;
 } MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
 
+// 定义一个宏，用于执行内存管理单元的检查，始终返回 MMU_DIRECT。
 #define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
 
 #endif
+
