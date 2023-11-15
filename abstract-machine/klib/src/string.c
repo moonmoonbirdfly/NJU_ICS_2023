93343d9389f8c8ec0a8bce85eff671083b9b678c
tracer-ics2023 <tracer@njuics.org>
2023-11-15 21:37:54 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:54 up  2:29,  1 user,  load average: 0.13, 0.15, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,31 +5,72 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+    const char *ptr = s;
+    while(*ptr != '\0') {
+    	ptr++;
+    }
+  return ptr - s;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+    char *origin_dst = dst;
+  while(*src != '\0') {
+    *dst = *src;
+    dst++;
+    src++;
+  }
+  *dst = '\0'; // add the null terminator
+  return origin_dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *origin_dst = dst;
+  size_t i;
+
+  // Copy up to n characters from the string pointed to by src to dst
+  for (i = 0; i < n && src[i] != '\0'; i++) {
+    dst[i] = src[i];
+  }
+
+  // If strlen(src) < n, pad the remaining with '\0'
+  for ( ; i < n; i++) {
+    dst[i] = '\0';
+  }
+
+  return origin_dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dest = dst;
+  while (*dst) {
+    dst++;
+  } 
+  while ((*dst++ = *src++));
+  return dest;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+    while (*s1 && (*s1 == *s2)) {
+    s1++, s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (; n > 0; s1++, s2++, --n){
+    if (*s1 != *s2)
+      return *(unsigned char *)s1 - *(unsigned char *)s2;
+    else if (*s1 == '\0')
+      return 0;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = (unsigned char *)s;
+  while(n--)
+    *p++ = (unsigned char)c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
