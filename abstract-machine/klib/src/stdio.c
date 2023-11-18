de706a391b573b25a586f1a07ce35d71ea1c25d9
tracer-ics2023 <tracer@njuics.org>
2023-11-18 18:09:20 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:20 up  1:17,  1 user,  load average: 0.43, 0.26, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,35 +4,55 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-static void reverse(char *s, int len) {
-  char* end = s + len - 1;
-  while (s < end) {
-    *s   = *s ^ *end;
-    *end = *s ^ *end;
-    *s   = *s ^ *end;
-    s++;
-    end--;
-  }
-}
+//static void reverse(char *s, int len) {
+//  char* end = s + len - 1;
+//  while (s < end) {
+//    *s   = *s ^ *end;
+//    *end = *s ^ *end;
+//    *s   = *s ^ *end;
+//    s++;
+//    end--;
+//  }
+//}
 
 
 /* itoa convert int to string under base. return string length */
-static int itoa(int n, char *s, int base) {
-assert(base <= 16);
-
-int i = 0, sign = n, bit;
-if (sign < 0) n = -n;
-do {
-bit = n % base;
-if (bit >= 10) s[i++] = 'a' + bit - 10;
-else s[i++] = '0' + bit;
-} while ((n /= base) > 0);
-if (sign < 0) s[i++] = '-';
-s[i] = '\0';
-reverse(s, i);
-
-return i;
+static char numToChar(int num) {
+    if (num < 10)
+        return num + '0';
+    else
+        return num - 10 + 'a';
 }
+
+static int itoa(int n, char *s, int base) {
+  assert(base <= 16);
+  
+  char buf[32];
+  int bit;
+  int i = 0;
+  int start = 0;
+  
+  if (n == 0) {
+    buf[i++] = '0';
+  } else {
+    while (n != 0) {
+      bit = n % base;
+      buf[i++] = numToChar(bit);
+      n /= base;
+    }
+  }
+
+  if (base == 10 && n < 0)
+    buf[i++] = '-';
+
+  while (i > 0) {
+    s[start++] = buf[--i];
+  }
+  s[start] = '\0';
+  
+  return start;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
