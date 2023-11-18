cd917f72f4d731145fab46fd90ca6ecde7494c7f
tracer-ics2023 <tracer@njuics.org>
2023-11-18 19:33:44 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:33:44 up  2:42,  1 user,  load average: 0.15, 0.15, 0.17

--- abstract-machine/am/src/native/ioe/timer.c
+++ abstract-machine/am/src/native/ioe/timer.c
@@ -24,7 +24,10 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   gettimeofday(&now, NULL);
   long seconds = now.tv_sec - boot_time.tv_sec;
   long useconds = now.tv_usec - boot_time.tv_usec;
-  uptime->us = seconds * 1000000 + (useconds + 500);
+  //uptime->us = seconds * 1000000 + (useconds + 500);
+  uint32_t low = inl(RTC_ADDR);
+  uint32_t high = inl(RTC_ADDR+4);
+  uptime->us = (uint64_t)low + (((uint64_t)high) << 32);
 }
 
 void __am_timer_init() {
