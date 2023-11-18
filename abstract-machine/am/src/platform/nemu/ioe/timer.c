396d3d327a96cec50e4ef9ae7724851510295dda
tracer-ics2023 <tracer@njuics.org>
2023-11-18 23:50:41 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:50:41 up  6:59,  1 user,  load average: 0.20, 0.19, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,17 +1,25 @@
 #include <am.h>
 #include <nemu.h>
 
+// 初始化 timer，将 timer 先设置为 0
 void __am_timer_init() {
-  outl(RTC_ADDR, 0);        
+  // 输出一串数据到 I/O 地址，这里是定时器的一个实现
+  // outl 函数将 value 的值写入 port 指定的 I/O 地址
+  outl(RTC_ADDR, 0);
   outl(RTC_ADDR + 4, 0);
 }
 
+// 获取从系统启动到现在的时间
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
- uptime->us = inl(RTC_ADDR + 4);
+  // 通过读取硬件 timer 的当前值，获取系统已经运行的时间
+  // inl 函数从 port 指定的 I/O 地址读取一个 dword（32位值）
+  uptime->us = inl(RTC_ADDR + 4);
+  // 由于 timer 的值可能会超过 32 位，所以这里需要读两次，然后将这两次的值合并成一个 64 位的整数
   uptime->us <<= 32;
   uptime->us += inl(RTC_ADDR);
 }
 
+// 获取现实世界的时间，这里并没有实现，只是简单地将所有字段都设为了固定值
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
@@ -20,3 +28,4 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->month  = 0;
   rtc->year   = 1900;
 }
+
--- abstract-machine/am/src/riscv/spike/timer.c
+++ abstract-machine/am/src/riscv/spike/timer.c
@@ -1,25 +1,38 @@
 #include <am.h>
 
+// 系统启动的时间点
 static uint64_t boot_time = 0;
 
+// 定义CLINT（Core Local Interruptor）的基地址，CLINT由RISC-V提供，包含了timer和software interrupt功能
 #define CLINT_MMIO 0x2000000ul
+
+// 指定一个地址偏移，用于读取 timer 的值
 #define TIME_BASE 0xbff8
 
+// 读取当前的时间，时间的单位是微秒(us)
 static uint64_t read_time() {
+  // 通过 MMIO 直接读取硬件 timer 的当前值。timer 的值是一个 64 位的整数，
+  // 分别通过两次 32 位的读操作来获取，然后合并为一个 64 位的整数
   uint32_t lo = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 0);
   uint32_t hi = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 4);
   uint64_t time = ((uint64_t)hi << 32) | lo;
+  // 由于实际得到的 timer 的值是以 1/10us 为单位的，所以这里需要除以10，得到以 us 为单位的时间
   return time / 10;
 }
 
+// 获取从系统启动至今的运行时间
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+  // 当前的时间减去系统启动的时间就是系统运行的时间
   uptime->us = read_time() - boot_time;
 }
 
+// 初始化 timer
 void __am_timer_init() {
+  // 准备记录系统启动的时间
   boot_time = read_time();
 }
 
+// 获取现实世界的时间，这里似乎并没有真正实现，只是简单地将所有字段都设为了0或1900
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
@@ -28,3 +41,4 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->month  = 0;
   rtc->year   = 1900;
 }
+
