69334617cce86b2cebb483baf9049e0ddbab3e64
tracer-ics2023 <tracer@njuics.org>
2023-11-18 23:15:46 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:15:46 up  6:24,  1 user,  load average: 1.30, 1.22, 0.90

--- abstract-machine/am/src/native/ioe/timer.c
+++ abstract-machine/am/src/native/ioe/timer.c
@@ -20,15 +20,20 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  long seconds = now.tv_sec - boot_time.tv_sec;
-  long useconds = now.tv_usec - boot_time.tv_usec;
-  uptime->us = seconds * 1000000 + (useconds + 500);
-  //uint32_t low = inl(RTC_ADDR);
-  //uint32_t high = inl(RTC_ADDR+4);
-  //uptime->us = (uint64_t)low + (((uint64_t)high) << 32);
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    long seconds = now.tv_sec - boot_time.tv_sec;
+    long useconds = now.tv_usec - boot_time.tv_usec;
+
+    // Correct for when useconds is negative
+    if (useconds < 0) {
+        seconds -= 1;  // subtract 1 second
+        useconds += 1000000;  // add 1 second in microseconds
+    }
+
+    uptime->us = seconds * 1000000 + useconds;
 }
+
 
 void __am_timer_init() {
   outl(RTC_ADDR, 0);        
