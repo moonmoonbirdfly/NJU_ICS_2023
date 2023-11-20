491e240deb2f287f3c3e3fc1c30064984b7e1e29
tracer-ics2023 <tracer@njuics.org>
2023-11-20 20:56:51 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:56:51 up  1:47,  1 user,  load average: 0.32, 0.21, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,26 +1,23 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
-// 初始化 timer，将 timer 先设置为 0
+#define RTC_LO_ADDR RTC_ADDR
+#define RTC_HI_ADDR (RTC_ADDR+4)
+
+// 这里假设0x48寄存器保存的时间单位是微秒
 void __am_timer_init() {
-  // 输出一串数据到 I/O 地址，这里是定时器的一个实现
-  // outl 函数将 value 的值写入 port 指定的 I/O 地址
-  outl(RTC_ADDR, 0);
-  outl(RTC_ADDR + 4, 0);
+   outl(RTC_LO_ADDR, 0);
+   outl(RTC_HI_ADDR, 0);
 }
 
-// 获取从系统启动到现在的时间
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // 通过读取硬件 timer 的当前值，获取系统已经运行的时间
-  // inl 函数从 port 指定的 I/O 地址读取一个 dword（32位值）
-  uptime->us <<= 32;
-  uptime->us = inl(RTC_ADDR + 4);
-  // 由于 timer 的值可能会超过 32 位，所以这里需要读两次，然后将这两次的值合并成一个 64 位的整数
-  
-  uptime->us += inl(RTC_ADDR);
+void __am_timer_uptime(uint64_t *uptime) {
+    uint32_t lo = inl(RTC_LO_ADDR);
+    uint32_t hi = inl(RTC_HI_ADDR);
+    // 注意这里是把高位数值左移32位，与低位做或操作，得到64位的时间值
+    *uptime = ((uint64_t)hi << 32) | lo;
 }
 
-// 获取现实世界的时间，这里并没有实现，只是简单地将所有字段都设为了固定值
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
