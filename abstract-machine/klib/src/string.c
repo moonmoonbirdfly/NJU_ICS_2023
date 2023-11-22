97c2d712051a2642b1f6918dd81b886f42f53570
tracer-ics2023 <tracer@njuics.org>
2023-11-22 17:42:57 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:57 up  1:15,  1 user,  load average: 0.84, 0.43, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -92,7 +92,7 @@ void *memmove(void *dst, const void *src, size_t n) {
     }
     return dst;
 }
-
+/*
 void *memcpy(void *out, const void *in, size_t n) {
   char *dst = out;
   const char *src = in;
@@ -100,8 +100,8 @@ void *memcpy(void *out, const void *in, size_t n) {
     *dst++ = *src++;
   }
   return out;
-}
-/*
+}*/
+
 int memcmp(const void *s1, const void *s2, size_t n) {
    const unsigned char *p1 = s1, *p2 = s2;
   while(n--) {
@@ -113,5 +113,5 @@ int memcmp(const void *s1, const void *s2, size_t n) {
   }
   return 0;
 }
-*/
+
 #endif
