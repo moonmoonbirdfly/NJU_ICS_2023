599588cde8b2805a186ef47da70db990d50a324b
tracer-ics2023 <tracer@njuics.org>
2023-12-19 14:34:12 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:12 up 25 min,  1 user,  load average: 0.27, 0.28, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -48,17 +48,17 @@ void yield() {
 bool ienabled() {
   uintptr_t mstatus;
   asm volatile("csrr %0, mstatus" : "=r"(mstatus));
-  return (mstatus & MSTATUS_MIE) != 0;
+  return (mstatus & MSTATUS_MXR) != 0;
 }
 
 void iset(bool enable) {
   uintptr_t mstatus;
   if (enable) {
     // Set the MIE bit to enable interrupts
-    asm volatile("csrrs %0, mstatus, %1" : "=r"(mstatus) : "r"(MSTATUS_MIE));
+    asm volatile("csrrs %0, mstatus, %1" : "=r"(mstatus) : "r"(MSTATUS_MXR));
   } else {
     // Clear the MIE bit to disable interrupts
-    asm volatile("csrrc %0, mstatus, %1" : "=r"(mstatus) : "r"(MSTATUS_MIE));
+    asm volatile("csrrc %0, mstatus, %1" : "=r"(mstatus) : "r"(MSTATUS_MXR));
   }
 }
 
