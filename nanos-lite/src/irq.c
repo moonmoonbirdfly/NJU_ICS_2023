c64b41f4c257fbb75e2920765f5244ce7709900c
tracer-ics2023 <tracer@njuics.org>
2023-12-20 17:01:39 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:39 up 19 min,  1 user,  load average: 0.12, 0.17, 0.17

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,9 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
-  printf("do_event中e.event=%d\n",e.event);
+ // printf("do_event中e.event=%d\n",e.event);
    switch (e.event) {
-    case 1:printf("event ID=%d\nc->GPRx=%d\n",e.event,c->GPRx);halt(0);printf("执行了halt之后");break;//EVENT_YIELD
+    case 1:printf("event ID=%d\n",e.event);break;
     //case 2:do_syscall(c);break;//EVENT_SYSCALL
     default: panic("Unhandled event ID = %d", e.event);break;
    }
