ee5f1f21edbb95051486d107a25e12dad819be8e
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:44:10 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:10 up  6:10,  1 user,  load average: 0.20, 0.18, 0.17

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,20 +15,43 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  for (size_t i = 0; i < len; ++i) putch(*((char *)buf + i));
+  return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+    AM_INPUT_KEYBRD_T t = io_read(AM_INPUT_KEYBRD);
+    if (t.keycode == AM_KEY_NONE) {
+    *(char*)buf = '\0';
+    return 0;
+  }
+   return snprintf((char *)buf, len, "%s %s\n", 
+    t.keydown ? "kd" : "ku",
+    keyname[t.keycode]);
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
+  return snprintf((char *)buf, len, "WIDTH : %d\nHEIGHT : %d", t.width, t.height);
 }
 
+
+//buf中的len字节写到屏幕上offset处
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
-}
+  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  int width = ev.width;
+
+  offset /= 4;
+  len /= 4;
+
+  int y = offset / width;
+  int x = offset - y * width;
+
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
+
+  return len;
+} 
+
 
 void init_device() {
   Log("Initializing devices...");
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -2,16 +2,18 @@
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
-
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, DEV_EVENTS, PROC_DISPINFO,FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -28,9 +30,97 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
-  
+  [DEV_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
+  [PROC_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
+  [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
 #include "files.h"
 };
+#define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+int fs_open(const char *pathname, int flags, int mode){
+  for (int i = 0; i < NR_FILES; i++) {
+        if (strcmp(file_table[i].name, pathname) == 0) {
+            file_table[i].open_offset = 0;
+            return i;
+        }
+    }
+  panic("file %s not found", pathname);
+}
+
+
+int fs_close(int fd){
+  return 0;
+}
+
+size_t fs_read(int fd, void *buf, size_t len){
+  ReadFn readFn = file_table[fd].read;
+  if (readFn != NULL) {
+        // 特殊文件处理
+        size_t open_offset = file_table[fd].open_offset;
+        return readFn(buf, open_offset, len);
+  }
+  size_t read_len = len;
+  size_t open_offset = file_table[fd].open_offset;
+  size_t size = file_table[fd].size;
+  size_t disk_offset = file_table[fd].disk_offset;
+  if (open_offset > size) return 0;
+  if (open_offset + len > size) read_len = size - open_offset;
+  ramdisk_read(buf, disk_offset + open_offset, read_len);
+  file_table[fd].open_offset += read_len;
+  return read_len;
+
+}
+size_t fs_write(int fd, const void *buf, size_t len) {
+    if (fd == 0) {
+        Log("ignore write %s", file_table[fd].name);
+        return 0;
+    }
+
+    WriteFn writeFn = file_table[fd].write;
+    if (writeFn != NULL) {
+        // 特殊文件处理
+      size_t open_offset = file_table[fd].open_offset;
+      return writeFn(buf, open_offset, len);
+    }
+    size_t write_len = len;
+    size_t open_offset = file_table[fd].open_offset;
+    size_t size = file_table[fd].size;
+    size_t disk_offset = file_table[fd].disk_offset;
+    if (open_offset > size) return 0;
+    if (open_offset + len > size) write_len = size - open_offset;
+    ramdisk_write(buf, disk_offset + open_offset, write_len);
+    file_table[fd].open_offset += write_len;
+    return write_len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence){
+  if (fd <= 2) {
+        Log("ignore lseek %s", file_table[fd].name);
+        return 0;
+  }
+
+  Finfo *file = &file_table[fd];
+  size_t new_offset;
+  // 根据 whence 参数来计算新的指针位置
+    if (whence == SEEK_SET) {
+        new_offset = offset;
+    } else if (whence == SEEK_CUR) {
+        new_offset = file->open_offset + offset;
+    } else if (whence == SEEK_END) {
+        new_offset = file->size + offset;
+    } else {
+        Log("Invalid whence value: %d", whence);
+        return -1;
+    }
+     // 检查新的指针位置是否在文件范围内
+    if (new_offset < 0 || new_offset > file->size) {
+        Log("Seek position out of bounds");
+        return -1;
+    }
+     // 设置新的文件读写指针
+    file->open_offset = new_offset;
+    
+    return new_offset;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
