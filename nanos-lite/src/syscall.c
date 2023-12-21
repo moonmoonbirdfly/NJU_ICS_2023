2717b59f1eef5df59f7da264082d5eb8bd561687
tracer-ics2023 <tracer@njuics.org>
2023-12-21 16:34:28 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:28 up  3:00,  1 user,  load average: 0.18, 0.17, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,7 +14,7 @@ int sys_write(intptr_t *buf, size_t count){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; //#define GPR1 gpr[17] // a7
- // intptr_t ret=0;
+  intptr_t ret=0;
  //printf("执行到do_syscall,此时根据c->GPR1的值来判断属于哪个系统调用 c->GPR1=a7=%d\n",a[0]);
   switch (a[0]) {
     case SYS_exit:c->GPRx=0;printf("SYS_exit， do_syscall此时 c->GPRx=%d\n",c->GPRx);halt(c->GPRx);//SYS_exit系统调用
@@ -27,4 +27,5 @@ void do_syscall(Context *c) {
     break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  c->GPRx=ret;
 }
