ff5b07182688a9f52535eaecf5acedc8d3976aef
tracer-ics2023 <tracer@njuics.org>
2023-12-21 14:26:30 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:30 up 52 min,  1 user,  load average: 0.20, 0.20, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,10 +7,6 @@ static Context *do_event(Event e, Context *c)
   switch (e.event)
   {
   case EVENT_YIELD:
-  case EVENT_IRQ_TIMER:
-    // printf("exchange process\n");
-    //c = schedule(c);
-    break;
   case EVENT_SYSCALL:
     do_syscall(c);
     break;
