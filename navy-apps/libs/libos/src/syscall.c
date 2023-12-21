663e5ec4f4d96ed244fa9ff3f00e2df48d136b28
tracer-ics2023 <tracer@njuics.org>
2023-12-21 14:19:13 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:13 up 45 min,  1 user,  load average: 0.15, 0.19, 0.18

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -60,17 +60,15 @@ void _exit(int status) {
   while (1);
 }
 
+
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  int ret = _syscall_(SYS_open, (intptr_t)path, flags, mode);
+  return ret;
 }
 
-int _write(int fd, void *buf, size_t count) {
-	assert(fd == 1 || fd == 2);
-  _syscall_(SYS_write, (intptr_t)buf, count,0);
-  
-  return count;
 
+int _write(int fd, void *buf, size_t count) {
+  return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
 extern char end;
