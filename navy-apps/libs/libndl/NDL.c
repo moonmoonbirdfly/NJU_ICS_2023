41cb90d4b8e94d686a34bc998159ab2236c88e5d
tracer-ics2023 <tracer@njuics.org>
2023-12-22 00:39:40 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:39:40 up 11:06,  1 user,  load average: 0.22, 0.18, 0.21

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -19,7 +19,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
    memset(buf, 0, len);
-  int fd = open("/dev/events", 0, 0);
+  int fd = open("/dev/events", 0, O_RDONLY);
   int ret = read(fd, buf, len);
   assert(close(fd) == 0);
   return ret == 0 ? 0 : 1;
