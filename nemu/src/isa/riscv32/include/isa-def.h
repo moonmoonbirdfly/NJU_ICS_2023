faa7d7ef7a3c1005491712c2abbb04589cd47599
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:01:51 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:01:51 up  5:28,  1 user,  load average: 0.43, 0.28, 0.21

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -29,6 +29,7 @@ typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)]; 
   vaddr_t pc; 
   MUXDEF(CONFIG_RV64, riscv64_CSRs, riscv32_CSRs) csr;
+  bool INTR; 
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,19 +14,44 @@
 ***************************************************************************************/
 
 #include <isa.h>
+
 #define MPIE_OFFSET 7
 #define MIE_OFFSET 3
+#define IRQ_TIMER 0x80000007
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
- 	if(NO==0){
-	  epc+=4;
-	}
+  /* TODO: Trigger an interrupt/exception with ``NO''.
+   * Then return the address of the interrupt/exception vector.
+   */
+  // 保存当前的 MIE 到 MPIE
+  cpu.csr.mstatus = (cpu.csr.mstatus & ~(1 << MPIE_OFFSET)) | ((cpu.csr.mstatus >> MIE_OFFSET) & 0x1) << MPIE_OFFSET ;
+    
+    // 将 MIE 置为 0
+  cpu.csr.mstatus &= ~(1 << MIE_OFFSET);
+  
+  epc+=4;
+  
   cpu.csr.mcause = NO;
   cpu.csr.mepc = epc;
-
+   
   return cpu.csr.mtvec;
 }
 
 
+
+void restore_interrupt() { 
+  // 还原之前保存的 MPIE 到 MIE
+  cpu.csr.mstatus = (cpu.csr.mstatus & ~(1 << MIE_OFFSET)) | ((cpu.csr.mstatus >> MPIE_OFFSET) & 0x1)<< MIE_OFFSET;
+  // 将 MPIE 位置为 1
+  cpu.csr.mstatus |= (1 << MPIE_OFFSET);
+
+}
+
+
+
 word_t isa_query_intr() {
-  return INTR_EMPTY;
+    if ((cpu.csr.mstatus & (1 << MIE_OFFSET))&& cpu.INTR) { 
+        cpu.INTR = false;
+        return IRQ_TIMER;
+    }
+    return INTR_EMPTY; 
 }
