d3a52e9bf99d01d4f07dab861eeaf61544d22819
tracer-ics2023 <tracer@njuics.org>
2023-12-21 19:47:21 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:47:21 up  6:13,  1 user,  load average: 0.30, 0.24, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include <proc.h>
 #include <elf.h>
-
+#include <fs.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -10,7 +10,7 @@
 #endif
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
-
+/*
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
@@ -28,8 +28,45 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
   return ehdr.e_entry;
 }
+*/
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  
+  int fd = fs_open(filename, 0, 0);
+  if (fd < 0) {
+    panic("should not reach here");
+  }
+  Elf_Ehdr elf;
 
+  assert(fs_read(fd, &elf, sizeof(elf)) == sizeof(elf));
+  // 检查魔数
+  assert(*(uint32_t *)elf.e_ident == 0x464c457f);
+  
+  Elf_Phdr phdr;
+  for (int i = 0; i < elf.e_phnum; i++) {
+    uint32_t base = elf.e_phoff + i * elf.e_phentsize;
 
+    fs_lseek(fd, base, 0);
+    assert(fs_read(fd, &phdr, elf.e_phentsize) == elf.e_phentsize);
+    
+    // 需要装载的段
+    if (phdr.p_type == PT_LOAD) {
+
+      char * buf_malloc = (char *)malloc(phdr.p_filesz);
+
+      fs_lseek(fd, phdr.p_offset, 0);
+      assert(fs_read(fd, buf_malloc, phdr.p_filesz) == phdr.p_filesz);
+      
+      memcpy((void*)phdr.p_vaddr, buf_malloc, phdr.p_filesz);
+      memset((void*)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_filesz);
+      
+      free(buf_malloc);
+    }
+  }
+
+  assert(fs_close(fd) == 0);
+  
+  return elf.e_entry;
+}
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
