72c9eac141a45c3686e5a355a22cc6e793117fca
tracer-ics2023 <tracer@njuics.org>
2023-12-21 13:40:48 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:48 up 7 min,  1 user,  load average: 0.28, 0.23, 0.12

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,24 @@
 #include <common.h>
 void do_syscall(Context *c);
+Context* schedule(Context *prev);
 static Context* do_event(Event e, Context* c) {
-  //printf("do_event中e.event=%d\n",e.event);
-   switch (e.event) {
-    case 1:printf("event ID=%d\nc->GPRx=%d\n",e.event,(int)c->GPRx);halt(0);break;//EVENT_YIELD
-    case 2:do_syscall(c);break;//EVENT_SYSCALL
-    default: panic("Unhandled event ID = %d", e.event);break;
-   }
-  //返回输入的上下文 c，表示处理事件后的上下文状态。
+  switch (e.event) {
+    case EVENT_YIELD:
+      // printf("EVENT_YIELD\n");
+      return schedule(c); //存起来了现在的，返回了另一个
+
+    case EVENT_SYSCALL:
+      do_syscall(c);
+      break;
+
+    case EVENT_IRQ_TIMER:
+      //Log("EVENT_IRQ_TIMER happening...");
+      //break;
+      return schedule(c);
+
+    default: panic("Unhandled event ID = %d", e.event);
+  }
+
   return c;
 }
 void init_irq(void) {
