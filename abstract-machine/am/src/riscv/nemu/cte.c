8a357dde107d15bff8dedcb3337085387d33efcd
tracer-ics2023 <tracer@njuics.org>
2023-12-21 14:21:15 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:15 up 47 min,  1 user,  load average: 0.11, 0.16, 0.17

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -2,61 +2,53 @@
 #include <riscv/riscv.h>
 #include <klib.h>
 
-
-#define Machine_Software_Interrupt (11)
-#define User_Software_Interrupt (8)
-#define IRQ_TIMER 0x80000007
-
-#define KERNEL 3
-#define USER 0
-
 static Context* (*user_handler)(Event, Context*) = NULL;
 
-void __am_get_cur_as(Context *c);
-void __am_switch(Context *c);
-
-Context* __am_irq_handle(Context *c) {
-  uintptr_t mscratch;
-  uintptr_t kas = 0;
-  asm volatile("csrr %0, mscratch" : "=r"(mscratch));
-  c->np = (mscratch == 0 ? KERNEL : USER);
-  asm volatile("csrw mscratch, %0" : : "r"(kas));
-
-  if ((uintptr_t)&c < 0x80000000){
-    halt(10001);
-  }
-  __am_get_cur_as(c);
-  //printf("__am_irq_handle c->pdir内容地址修改前 页表项:%p\t上下文地址%p\t所在栈帧:%p\n", c->pdir, c, &c);
-  //printf("设置c->np为%d\n", c->np);
-  if (user_handler) {
+Context *__am_irq_handle(Context *c)
+{
+  // __am_get_cur_as(c);
+  // display_context(c);
+  // printf("pte is at %p\n", c->pdir);
+  // printf("cause to interrupt is %d\n", c->GPR1);
+  // display_context(c);
+  if (user_handler)
+  {
+    // printf("the exception cause id is %d\n", c->mcause);
     Event ev = {0};
-    
-    switch (c->mcause) {
-      case Machine_Software_Interrupt:
-      case User_Software_Interrupt:
-        // printf("c->GPR1 = %d \n", c->GPR1);
-        if (c->GPR1 == -1){ // 特指-1
-          ev.event = EVENT_YIELD;
-        }else {
-          ev.event = EVENT_SYSCALL;
-        }
-        c->mepc += 4;
-        break;
 
-      case IRQ_TIMER:
-        ev.event = EVENT_IRQ_TIMER;
-        break;
-
-      default: ev.event = EVENT_ERROR; break;
+    switch (c->mcause)
+    {
+    case -1:
+      ev.event = EVENT_YIELD;
+      break;
+    case 0 ... 19:
+      ev.event = EVENT_SYSCALL;
+      break;
+    case 0x80000007:
+      ev.event = EVENT_IRQ_TIMER;
+      break;
+    default:
+      ev.event = EVENT_ERROR;
+      break;
     }
 
+    // printf("event id is %d\n",ev.event);
+    // printf("here\n");
     c = user_handler(ev, c);
+    // printf("===============");
+    // printf("mepc is %p\n", c->mepc);
+    // printf("c is at %p\n",c);
     assert(c != NULL);
   }
-
-  //printf("__am_irq_handle c->pdir内容地址修改后 页表项:%p\t上下文地址%p\t所在栈帧:%p\n", c->pdir, c, &c);
-  __am_switch(c);
-  //printf("c->np为%d\n", c->np);
+  // printf("ret from interrupt is %p\n", c->GPRx);
+  // ((void (*)())c->mepc)();
+  // c->gpr[0] = 0;
+  // printf("pte is at %p after change\n", c->mepc);
+  // printf("sp is %p\n", c->gpr[2]);
+  // display_context(c);
+  assert(c->mepc >= 0x40000000 && c->mepc <= 0x88000000);
+  // __am_switch(c);
+  // printf("entry is %p\n", c->mepc);
   return c;
 }
 
@@ -73,24 +65,15 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  //-4是为了缓存一个t0，为了Real VME
-  uintptr_t *t0_buf = kstack.end - 4;
-  *t0_buf = 0;
-
-  Context *context = kstack.end - sizeof(Context) - 4;
-  context->mstatus = 0x1800 | 0x80;
-  context->mepc    = (uintptr_t)entry;
-  context->gpr[10] = (uintptr_t)arg;
-  context->pdir    = NULL;
-  //为了Real VME
-  context->np      = 3;
-  context->gpr[2]  = (uintptr_t)kstack.end - 4;
-  //TODO: 还需要添加一些
-  return context;
+  return NULL;
 }
 
 void yield() {
+#ifdef __riscv_e
+  asm volatile("li a5, -1; ecall");
+#else
   asm volatile("li a7, -1; ecall");
+#endif
 }
 
 bool ienabled() {
@@ -99,3 +82,4 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
+
