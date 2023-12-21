d2557ddf018d5bf57a19f331267fe2ba91739745
tracer-ics2023 <tracer@njuics.org>
2023-12-21 14:20:19 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:19 up 46 min,  1 user,  load average: 0.08, 0.16, 0.17

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -2,25 +2,61 @@
 #include <riscv/riscv.h>
 #include <klib.h>
 
+
+#define Machine_Software_Interrupt (11)
+#define User_Software_Interrupt (8)
+#define IRQ_TIMER 0x80000007
+
+#define KERNEL 3
+#define USER 0
+
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+void __am_get_cur_as(Context *c);
+void __am_switch(Context *c);
+
 Context* __am_irq_handle(Context *c) {
- //printf("mcause  %d \n",(int)c->mcause);
- //panic("mcause %d",(int)c->mcause);
+  uintptr_t mscratch;
+  uintptr_t kas = 0;
+  asm volatile("csrr %0, mscratch" : "=r"(mscratch));
+  c->np = (mscratch == 0 ? KERNEL : USER);
+  asm volatile("csrw mscratch, %0" : : "r"(kas));
+
+  if ((uintptr_t)&c < 0x80000000){
+    halt(10001);
+  }
+  __am_get_cur_as(c);
+  //printf("__am_irq_handle c->pdir内容地址修改前 页表项:%p\t上下文地址%p\t所在栈帧:%p\n", c->pdir, c, &c);
+  //printf("设置c->np为%d\n", c->np);
   if (user_handler) {
     Event ev = {0};
+    
     switch (c->mcause) {
-      case -1:
-        ev.event=EVENT_YIELD;break;
-        case 0:case 1:case 2:case 3:case 4:case 5:case 6:case 7:case 8:case 9:case 10:case 11:case 12:case 13:case 14:case 15:case 16:case 17:case 18:case 19:ev.event=EVENT_SYSCALL;break;
+      case Machine_Software_Interrupt:
+      case User_Software_Interrupt:
+        // printf("c->GPR1 = %d \n", c->GPR1);
+        if (c->GPR1 == -1){ // 特指-1
+          ev.event = EVENT_YIELD;
+        }else {
+          ev.event = EVENT_SYSCALL;
+        }
+        c->mepc += 4;
+        break;
+
+      case IRQ_TIMER:
+        ev.event = EVENT_IRQ_TIMER;
+        break;
 
       default: ev.event = EVENT_ERROR; break;
     }
-    //user_handler是cte_init中注册的回调函数
+
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  
+
+  //printf("__am_irq_handle c->pdir内容地址修改后 页表项:%p\t上下文地址%p\t所在栈帧:%p\n", c->pdir, c, &c);
+  __am_switch(c);
+  //printf("c->np为%d\n", c->np);
   return c;
 }
 
@@ -37,15 +73,24 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  //-4是为了缓存一个t0，为了Real VME
+  uintptr_t *t0_buf = kstack.end - 4;
+  *t0_buf = 0;
+
+  Context *context = kstack.end - sizeof(Context) - 4;
+  context->mstatus = 0x1800 | 0x80;
+  context->mepc    = (uintptr_t)entry;
+  context->gpr[10] = (uintptr_t)arg;
+  context->pdir    = NULL;
+  //为了Real VME
+  context->np      = 3;
+  context->gpr[2]  = (uintptr_t)kstack.end - 4;
+  //TODO: 还需要添加一些
+  return context;
 }
 
 void yield() {
-#ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
-#else
   asm volatile("li a7, -1; ecall");
-#endif
 }
 
 bool ienabled() {
@@ -54,4 +99,3 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
-
