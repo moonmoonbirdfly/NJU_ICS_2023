3000114336e44d1c5469107b4610fc552516aa35
tracer-ics2023 <tracer@njuics.org>
2023-12-22 12:21:56 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:21:56 up 38 min,  1 user,  load average: 0.48, 0.32, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,17 +18,17 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   for (size_t i = 0; i < len; ++i) putch(*((char *)buf + i));
   return len;
 }
-
 size_t events_read(void *buf, size_t offset, size_t len) {
-    AM_INPUT_KEYBRD_T t = io_read(AM_INPUT_KEYBRD);
-    if (t.keycode == AM_KEY_NONE) {
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) {
     *(char*)buf = '\0';
     return 0;
   }
-   return snprintf((char *)buf, len, "%s %s\n", 
-    t.keydown ? "kd" : "ku",
-    keyname[t.keycode]);
+  int ret = snprintf(buf, len, "%s %s\n", ev.keydown?"kd":"ku", keyname[ev.keycode]);
+  printf("%s\n", buf);
+  return ret;
 }
+
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
