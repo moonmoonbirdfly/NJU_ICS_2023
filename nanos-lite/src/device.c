5ebbb7131a1ab1e4d4c1f6f931dd78ce98e811e7
tracer-ics2023 <tracer@njuics.org>
2023-12-22 14:02:33 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:33 up  2:19,  1 user,  load average: 0.45, 0.23, 0.13

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,11 +20,12 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  
+ /* 
   if (ev.keycode == AM_KEY_NONE) {
     *(char*)buf = '\0';
     return 0;
   }
+  */
   //printf("%s\n", ev.keycode);
   int ret = snprintf(buf, len, "%s %s\n", ev.keydown?"kd":"ku", keyname[ev.keycode]);
   printf("%s\n", ret);
