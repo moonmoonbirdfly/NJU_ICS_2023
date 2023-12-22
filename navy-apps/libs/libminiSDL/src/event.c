620441807c5acc301e9350b3575e2bb3e5487d79
tracer-ics2023 <tracer@njuics.org>
2023-12-22 11:55:41 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:55:41 up 12 min,  1 user,  load average: 0.14, 0.19, 0.14

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -8,12 +8,60 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PollEvent(SDL_Event *ev) {
+  unsigned buf_size = 32;
+  char *buf = (char *)malloc(buf_size * sizeof(char));
+  if (NDL_PollEvent(buf, buf_size) == 1) {
+      if (strncmp(buf, "kd", 2) == 0) {
+          ev->key.type = SDL_KEYDOWN;
+      } else {
+          ev->key.type = SDL_KEYUP;
+      }
+
+      int flag = 0;
+      for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
+          if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
+                  && strlen(keyname[i]) == strlen(buf) - 4) {
+              flag = 1;
+              ev->key.keysym.sym = i;
+              break;
+          }
+      }
+
+      
+      free(buf);
+      return 1;
+  } else {
+      return 0;
+  }
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+int SDL_WaitEvent(SDL_Event *ev) {
+    unsigned buf_size = 32;
+    char *buf = (char *)malloc(buf_size * sizeof(char));
+
+    while (NDL_PollEvent(buf, buf_size) == 0); // wait ...
+
+    if (strncmp(buf, "kd", 2) == 0)
+        ev->key.type = SDL_KEYDOWN;
+    else
+        ev->key.type = SDL_KEYUP;
+
+
+    int flag = 0;
+    for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
+        if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
+                && strlen(keyname[i]) == strlen(buf) - 4) {
+            flag = 1;
+            ev->key.keysym.sym = i;
+            break;
+        }
+    }
+
+   
+
+    free(buf);
+    return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
@@ -24,6 +72,13 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
   return 0;
 }
 
+static unsigned char keystate[sizeof(keyname) / sizeof(keyname[0])];
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+    SDL_Event ev;
+    if (SDL_PollEvent(&ev) == 1 && ev.key.type == SDL_KEYDOWN) {
+        keystate[ev.key.keysym.sym] = 1;
+    } else {
+        memset(keystate, 0, sizeof(keystate));
+    }
+    return keystate;
 }
