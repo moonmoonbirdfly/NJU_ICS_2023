eadf1eb94757d2a843f2d00c2df4457a92e5bf95
tracer-ics2023 <tracer@njuics.org>
2023-12-23 13:40:47 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:47 up 6 min,  1 user,  load average: 0.20, 0.21, 0.11

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,6 +26,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     *(char*)buf = '\0';
     return 0;
   }
+  printf("1");
    return snprintf((char *)buf, len, "%s %s\n", t.keydown ? "kd" : "ku",keyname[t.keycode]);
 }
 
