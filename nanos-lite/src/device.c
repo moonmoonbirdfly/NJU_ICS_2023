5a8b51bf477503eb4d1050cabd8bd05861ffce60
tracer-ics2023 <tracer@njuics.org>
2023-12-23 13:53:33 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:53:33 up 19 min,  1 user,  load average: 0.54, 0.43, 0.31

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   for (size_t i = 0; i < len; ++i) putch(*((char *)buf + i));
   return len;
 }
-
 size_t events_read(void *buf, size_t offset, size_t len) {
     AM_INPUT_KEYBRD_T t = io_read(AM_INPUT_KEYBRD);
     if (t.keycode == AM_KEY_NONE) {
@@ -31,6 +30,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     t.keydown ? "kd" : "ku",
     keyname[t.keycode]);
 }
+
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
@@ -53,6 +53,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
   return len;
 } 
+
 
 void init_device() {
   Log("Initializing devices...");
