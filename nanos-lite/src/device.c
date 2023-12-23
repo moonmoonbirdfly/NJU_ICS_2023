d3c832dac0c5be50864070e455cb5440d9541a53
tracer-ics2023 <tracer@njuics.org>
2023-12-23 13:35:24 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:35:24 up 1 min,  1 user,  load average: 0.54, 0.23, 0.09

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,52 +20,13 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   for (size_t i = 0; i < len; ++i) putch(*((char *)buf + i));
   return len;
 }
-void switch_program_index(int new_index);
-
-//offset被忽视
 size_t events_read(void *buf, size_t offset, size_t len) {
-  //yield();
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE) return 0;
-  
-  switch (ev.keycode){
-  case AM_KEY_F1:
-    switch_program_index(1);
+    AM_INPUT_KEYBRD_T t = io_read(AM_INPUT_KEYBRD);
+    if (t.keycode == AM_KEY_NONE) {
+    *(char*)buf = '\0';
     return 0;
-
-  case AM_KEY_F2:
-    switch_program_index(2);
-    return 0;
-
-  case AM_KEY_F3:
-    switch_program_index(3);
-    return 0;
-  
-  default:
-    break;
   }
-
-  //int real_length = 4;
-  char *tag = ev.keydown ? "kd " : "ku ";
-  //if (real_length <= len){
-  strcpy(buf, tag);
-  // }else {
-  //   assert(0);
-  //   return 0;
-  // }
-  
-  //real_length += strlen(keyname[ev.keycode]);
-  
-  //if (real_length<= len){
-  strcat(buf, keyname[ev.keycode]);
-  // }else {
-  //   Log("Need %d for %s%s but got %d", strlen(keyname[ev.keycode]), (char *)buf, keyname[ev.keycode], len);
-  //   assert(0);
-  //   return 0;
-  // }
-  Log("Got  (kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
-  
-  return 1;
+   return snprintf((char *)buf, len, "%s %s\n", t.keydown ? "kd" : "ku",keyname[t.keycode]);
 }
 
 
