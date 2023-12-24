7249663658f677639ed2c3c0409ddc8e8da15817
tracer-ics2023 <tracer@njuics.org>
2023-12-24 14:39:35 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:39:35 up  2:16,  1 user,  load average: 0.29, 0.26, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -54,7 +54,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-	const char filename[] = "/bin/menu";
+	const char filename[] = "/bin/event-test";
   naive_uload(NULL, filename);
 }
 
