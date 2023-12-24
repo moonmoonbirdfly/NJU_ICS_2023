e44ca5ff27dd02dec3025bb0dfea25c22bfddc45
tracer-ics2023 <tracer@njuics.org>
2023-12-24 16:27:25 +0800

    >  compile NEMU 221830013 曾鹏飞 Linux dc 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:25 up  1:32,  1 user,  load average: 0.84, 0.44, 0.40

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,27 +40,19 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 //buf中的len字节写到屏幕上offset处
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  // 从GPU配置获取屏幕宽度和高度
-  AM_GPU_CONFIG_T config = io_read(AM_GPU_CONFIG);
-  int width = config.width;
-  int height = config.height;
+  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  int width = ev.width;
 
-  // 计算开始绘制的位置和像素
-  int x = offset % width; // 行内偏移
-  int y = offset / width; // 行偏移
-  if (y >= height) return 0; // 如果偏移超过屏幕尺寸，不进行任何绘制
+  offset /= 4;
+  len /= 4;
 
-  // 计算实际可以绘制的字节数
-  size_t bytes_writeable = (len + offset > width * height * 4) ? 
-                           (width * height * 4 - offset) : len;
-  size_t pixels_writeable = bytes_writeable / 4; // 将字节转换为像素数
+  int y = offset / width;
+  int x = offset - y * width;
 
-  // 使用io_write绘制到屏幕上
-  io_write(AM_GPU_FBDRAW, x, y,(void*) buf, pixels_writeable, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
 
-  return bytes_writeable; // 返回实际写入的字节数
-}
-
+  return len;
+} 
 
 
 void init_device() {
